bf7c5b26f24205548e28b95572305d7df43e4f62
Alan Aguiar <alanjas@hotmail.com>
2012-11-14 21:33:50 -0200

    use another path to output

--- merge.py
+++ merge.py
@@ -3,15 +3,21 @@ import os
 import sys
 import glob
 import subprocess
+DEFAULT = True
 
 print 'Merging strings of turtle-extras with turtleblocks'
 
 if len(sys.argv) == 2:
     if sys.argv[1] == 'turtlebots':
         print 'Using TurtleBots-style paths'
+        DEFAULT = False
         path = os.path.join(
                 os.path.abspath('..'),
                 'mainline',
+                'po')
+        path_tmp = os.path.join(
+                os.path.abspath('..'),
+                'tmp',
                 'po')
 else:
     print 'Using default paths'
@@ -27,10 +33,17 @@ for pof in po_files:
     taf = os.path.expanduser(os.path.join(
             path,
             os.path.basename(pof)))
+    taf_output = taf
+
+    if not(DEFAULT):
+            taf_output = os.path.expanduser(os.path.join(
+            path_tmp,
+            os.path.basename(pof)))
 
     if os.path.exists(taf):
-        command_line = ['msgcat', pof, taf, '-o', taf]
+        command_line = ['msgcat', pof, taf, '-o', taf_output]
         if subprocess.call(command_line) > 0:
             print 'error processing', pof
     else:
         print 'skipping', taf
+
